390d21a327b9125e0550fb34b8c8433ca8d7ccef
Shima Shima <shimashima8293@gmail.com>
2023-06-30 15:53:00 +0900

    Updated comments in `resource.h`

--- GameEngine/source/resource.h
+++ GameEngine/source/resource.h
@@ -6,17 +6,17 @@
 #include <string>
 #include <vector>
 
-// Open a font. It will Print out an error message if it failed to load.
+// Open a font. It will Print out an error message if it failed to load. Don't forget to release the memory with `closeResources()` or `closeFont()`!
 // \param _font The loaded font to check.
 // \returns Returns a pointer to TTF_Font, which is required for `Entity::loadFromText()`.
 TTF_Font* openFont(std::string path, int size);
 
-// Open an audio file. It will Print out an error message if it failed to load.
+// Open an audio file. It will Print out an error message if it failed to load. Don't forget to release the memory with `closeResources()` or `closeMusic()`!
 // \param _music The loaded music to check.
 // \returns Returns a pointer to Mix_Music.
 Mix_Music* openMusic(std::string path);
 
-// Open a wave file. It will Print out an error message if it failed to load.
+// Open a wave file. It will Print out an error message if it failed to load. Don't forget to release the memory with `closeResources()` or `closeWAV()`!
 // \param _chunk The loaded chunk to check.
 // \returns Returns a pointer to Mix_Chunk, which is required for `playMusicSet()`
 Mix_Chunk* openWAV(std::string path);
